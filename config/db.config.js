66918bfdaf9c8d71cc4911b056f460502b6e2250
Mohd Arshad <mohdarshad2583@gmail.com>
2025-12-18 22:21:53 +0530

    food app backend

---
+++ config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // url: "mongodb://localhost:27017/gofood"
+    url: "mongodb+srv://mohdarshad2583_db_user:tj9haNN6PW29tmGn@cluster0.duwfhqc.mongodb.net/?appName=Cluster0"
+};
+
+
+
